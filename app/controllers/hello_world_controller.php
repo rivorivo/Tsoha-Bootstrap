a67c38b8595225c03634cfa9eb4b5ec2e798ca48
aceriku <rikuvo@gmail.com>
2016-04-02 13:56:51 +0300

    suunnitelmat

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,27 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html')
+    }
+
+    public static function recipe_list(){
+      View::make('suunnitelmat/recipe_list.html')
+    }
+    public static function recipe_show(){
+      View::make('suunnitelmat/recipe_show.html')
+    }
+    public static function edit_recipe(){
+      View::make('suunnitelmat/edit_recipe.html')
+    }
+     public static function login(){
+      View::make('suunnitelmat/login.html')
+    }
+     public static function home(){
+      View::make('suunnitelmat/home.html')
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,19 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/recipes', function(){
+  	HelloWorldController::recipe_list();
+  });
+  $routes->get('/login', function(){
+  	HelloWorldController::login();
+  });
+  $routes->get('/recipes/1', function(){
+  	HelloWorldController::recipe_show();
+  });
+  $routes->get('/recipes/2', function(){
+  	HelloWorldController::edit_recipe();
+  });
+  $routes->get('/home', function(){
+  	HelloWorldController::home();
+  });
